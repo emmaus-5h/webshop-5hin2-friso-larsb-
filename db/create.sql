513e0eed272493a520f8cdb0722ff0c1c34236bb
Lars4hf <93325257+Lars4hf@users.noreply.github.com>
2023-02-06 11:24:04 +0000

    fixed sql 3 tabellen

--- db/create.sql
+++ db/create.sql
@@ -2,13 +2,13 @@
 -- create tables
 --
 
---dranken
+--drankeninsert into products (id, name, description, code, price) values (1, 'malibu', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  code VARCHAR(15),
-  name VARCHAR(255),
+  code VARCHAR (15),
+  name VARCHAR (255),
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC (10,2)
 );
 
 
@@ -21,28 +21,80 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('malibu', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('whiskey', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('port', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('cognac', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('calvados', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('rum', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-insert into products (name, description, code, price) values ('ouzo', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('gin', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('limoncello', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('jenever', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
+insert into products (id, name, description, code, price) values (1, 'malibu', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
+insert into products (id, name, description, code, price) values (2, 'whiskey', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
+insert into products (id, name, description, code, price) values (3, 'port', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
+insert into products (id, name, description, code, price) values (4, 'cognac', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
+insert into products (id, name, description, code, price) values (5, 'calvados', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
+insert into products (id, name, description, code, price) values (6, 'rum', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (id, name, description, code, price) values (7, 'ouzo', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
+insert into products (id, name, description, code, price) values (8, 'gin', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
+insert into products (id, name, description, code, price) values (9, 'limoncello', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
+insert into products (id, name, description, code, price) values (10, 'jenever', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
 
+CREATE TABLE persons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name INTEGER,
+  persons_id INTEGER,
+  persons
+);
+
+insert into persons (id, name) values (1, 'jeroen');
+insert into persons (id, name) values (2, 'friso');
+insert into persons (id, name) values (3, 'lars');
+insert into persons (id, name) values (4, 'mark');
+insert into persons (id, name) values (5, 'sigrid kaag');
+insert into persons (id, name) values (6, 'daan');
+insert into persons (id, name) values (7, "kooskabouter");
+insert into persons (id, name) values (8, 'bananenliefhebber');
+insert into persons (id, name) values (9, 'godzilla');
+insert into persons (id, name) values (10, 'joost');
 
 CREATE TABLE revieuws (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   product_id INTEGER,
+  persons_id INTEGER,
   revieuws
+  persons
 );
 
-insert into revieuws (product_id, revieuws) values (1, 'nice');
-insert into revieuws (product_id, revieuws) values (2, 'basic');
-insert into revieuws (product_id, revieuws) values (3, 'romig');
-insert into revieuws (product_id, revieuws) values (1, 'lekker');
-insert into revieuws (product_id, revieuws) values (5, 'slecht');
-insert into revieuws (product_id, revieuws) values (6, 'goed');
-insert into revieuws (product_id, revieuws) values (4, "matig");
+insert into revieuws (product_id, persons_id, revieuws) values (1, 2, 'nice');
+insert into revieuws (product_id, persons_id, revieuws) values (2, 4, 'basic');
+insert into revieuws (product_id, persons_id, revieuws) values (3, 1, 'romig');
+insert into revieuws (product_id, persons_id, revieuws) values (4, 3, 'lekker');
+insert into revieuws (product_id, persons_id, revieuws) values (5, 6, 'slecht');
+insert into revieuws (product_id, persons_id, revieuws) values (6, 7, 'goed');
+insert into revieuws (product_id, persons_id, revieuws) values (7, 5, "matig");
+insert into revieuws (product_id, persons_id, revieuws) values (8, 8, 'hemel');
+insert into revieuws (product_id, persons_id, revieuws) values (9, 10, 'oh my days');
+insert into revieuws (product_id, persons_id, revieuws) values (10, 9, 'perfect');
+insert into revieuws (product_id, persons_id, revieuws) values (1, 10, 'geweldig');
+insert into revieuws (product_id, persons_id, revieuws) values (5, 9, 'heel lekker');
+insert into revieuws (product_id, persons_id, revieuws) values (6, 8, 'top');
+insert into revieuws (product_id, persons_id, revieuws) values (4, 7, "mooi");
+insert into revieuws (product_id, persons_id, revieuws) values (2, 6, 'geweldig');
+insert into revieuws (product_id, persons_id, revieuws) values (8, 5, 'heel lekker');
+insert into revieuws (product_id, persons_id, revieuws) values (3, 4, 'top');
+insert into revieuws (product_id, persons_id, revieuws) values (10, 3, "suuuiiiiii");
+insert into revieuws (product_id, persons_id, revieuws) values (7, 2, "god damn");
+insert into revieuws (product_id, persons_id, revieuws) values (9, 1, "amazing");
+insert into revieuws (product_id, persons_id, revieuws) values (10, 1,  'geweldig');
+insert into revieuws (product_id, persons_id, revieuws) values (6, 2, 'heel lekker');
+insert into revieuws (product_id, persons_id, revieuws) values (5, 3, 'top');
+insert into revieuws (product_id, persons_id, revieuws) values (2, 4, "mooi");
+insert into revieuws (product_id, persons_id, revieuws) values (5, 5, 'geweldig');
+insert into revieuws (product_id, persons_id, revieuws) values (3, 6, 'heel lekker');
+insert into revieuws (product_id, persons_id, revieuws) values (8, 7, 'top');
+insert into revieuws (product_id, persons_id, revieuws) values (9, 8, "pretty");
+insert into revieuws (product_id, persons_id, revieuws) values (1, 9, "god damn");
+insert into revieuws (product_id, persons_id, revieuws) values (5, 10, "amazing");
+insert into revieuws (product_id, persons_id, revieuws) values (6, 5, 'geweldig');
+insert into revieuws (product_id, persons_id, revieuws) values (1, 4, 'heel lekker');
+insert into revieuws (product_id, persons_id, revieuws) values (6, 3, 'top');
+insert into revieuws (product_id, persons_id, revieuws) values (4, 2, "mooi");
+insert into revieuws (product_id, persons_id,revieuws) values (9, 1, 'geweldig');
+insert into revieuws (product_id, persons_id, revieuws) values (8, 10, 'heel lekker');
+insert into revieuws (product_id, persons_id, revieuws) values (3, 9, 'top');
+insert into revieuws (product_id, persons_id, revieuws) values (10, 8, "pretty");
+insert into revieuws (product_id, persons_id, revieuws) values (7, 7, "god damn");
+insert into revieuws (product_id, persons_id, revieuws) values (2, 6, "amazing");
