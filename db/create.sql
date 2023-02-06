5307f7f6f55a183f4a80c689016454f1cde3cd22
Lars4hf <93325257+Lars4hf@users.noreply.github.com>
2023-02-06 10:22:55 +0000

    10 items

--- db/create.sql
+++ db/create.sql
@@ -27,6 +27,11 @@ insert into products (name, description, code, price) values ('port', 'Pellentes
 insert into products (name, description, code, price) values ('cognac', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
 insert into products (name, description, code, price) values ('calvados', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('rum', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('ouzo', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
+insert into products (name, description, code, price) values ('gin', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
+insert into products (name, description, code, price) values ('limoncello', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
+insert into products (name, description, code, price) values ('jenever', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
+
 
 CREATE TABLE revieuws (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
